db9afaad77607715915dd3f13acf339e32a494b4
lagebergerjensen <lage.berger.jensen@visolit.no>
2020-01-30 12:23:28 +0100

    initialize

---
+++ README.md
@@ -0,0 +1,10 @@
+# PSClientInstall
+
+This is a set of Powershell scripts that installs and configures most of the software I use on my computer for administration and development. Some things are slightly difficult to automate, so I still have some manual installation steps, but at least it's all documented here.
+
+This is a work in progress, and is mostly a means for me to document my current computer setup. I'll be evolving this set of scripts over time.
+
+See also:
+[Mac Development Ansible Playbook](https://github.com/geerlingguy/mac-dev-playbook) - the original inspiration for this repo.
+
+## Installation
---
+++ scripts/InstallRequirements.ps1
@@ -0,0 +1,19 @@
+﻿# Install Chocolatey.
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install pacakges
+choco install -y git.install --params '/GitAndUnixToolsOnPath /NoGitLfs /SChannel /NoAutoCrlf /WindowsTerminal /NoShellIntegration'
+choco install -y powershell-core
+choco install -y vscode
+
+# Create Powershell profile file.
+if (-not Test-Path -Path $profile) {
+    New-Item -Path $profile -ItemType File
+}
+
+# Populate $profile
+$profileSettings = @(
+    '$env:PSScriptPath = "$env:USERPROFILE\Documents\PowerShell\Scripts"',
+    '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12'
+)
+
